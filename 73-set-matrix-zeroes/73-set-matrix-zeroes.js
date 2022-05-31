1b5df8127e46c89a6d27cf526f9b2c83aa1f6f9e
Afzhal-ahmed-s <96117548+Afzhal-ahmed-s@users.noreply.github.com>
2022-05-31 14:17:17 +0530

    Time: 197 ms (5.11%), Space: 52.4 MB (5.11%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.js
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.js
@@ -3,30 +3,43 @@
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
 var setZeroes = function(matrix) {
-    let row=new Set(),col=new Set();
-    let m=matrix.length, n=matrix[0].length;
+    let firstrow=false,firstcol=false;
+    let rows=matrix.length, cols=matrix[0].length;
     
-    for(let i=0;i<m;i++){
-        for(let j=0;j<n;j++){
-
+    for(let i=0;i<cols;i++){
+        if(matrix[0][i]==0)firstrow=true;
+    }
+    
+      for(let i=0;i<rows;i++){
+        if(matrix[i][0]==0)firstcol=true;
+    }
+    
+    for(let i=1;i<rows;i++){
+        for(let j=1;j<cols;j++){
             if(matrix[i][j]==0){
-                row.add(i);
-                col.add(j);
+                matrix[i][0]=0;
+                matrix[0][j]=0;
             }
         }
     }
-
-    for(let i=0;i<m;i++){
-        if(row.has(i))matrix[i]=matrix[i].map(x=>x*0);
-    }
+    console.log(matrix);
     
-    for(let i=0;i<n;i++){
-        for(let j=0;j<m;j++){
-            if(col.has(i)){
-               matrix[j][i]=0;
+    for(let i=1;i<rows;i++){
+        for(let j=1;j<cols;j++){
+            if(matrix[0][j]==0 || matrix[i][0]==0){
+               matrix[i][j]=0;
             }
         }
     }
     
+    if(firstrow){
+        matrix[0]=matrix[0].map(x=>x*0);
+    }
+    
+    if(firstcol){
+        for(let i=0;i<rows;i++){
+            matrix[i][0]=0;
+        }
+    }
     return matrix;
 };
