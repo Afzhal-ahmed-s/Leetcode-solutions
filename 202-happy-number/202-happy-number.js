6bc4bbf8f773b45781cec08702789310de072e61
Afzhal-ahmed-s <96117548+Afzhal-ahmed-s@users.noreply.github.com>
2022-05-31 05:37:56 +0530

    Time: 79 ms (67.64%), Space: 44.5 MB (22.76%) - LeetHub

--- 202-happy-number/202-happy-number.js
+++ 202-happy-number/202-happy-number.js
@@ -3,24 +3,19 @@
  * @return {boolean}
  */
 var isHappy = function(n) {
-//   if (n === 1) return true;
-  
-//   let tortoise = n, hare = getNext(n);
-//   while (hare !== 1 && tortoise !== hare) {
-//     tortoise = getNext(tortoise);
-//     hare = getNext(getNext(hare));
-//   }
-  
-//   return tortoise !== hare;
-        
-//   function getNext(n) {
-//     return (n + '').split('').map(num => (num - 0)**2).reduce((sum, num) => sum += num)
-//   }
+
+    if(n<1)return false;
     
-    let cur = n, curSet = new Set();
-  while (cur !== 1 && !curSet.has(cur)) {
-    curSet.add(cur)
-    cur = (cur + '').split('').map(num => (num - 0)**2).reduce((sum, num) => sum += num);
-  }
-  return cur === 1;
+    let slow=n,fast=run(n);
+    while(slow!==fast && slow!==1){
+        slow=run(slow);
+        fast=run(run(fast));
+    }
+    if(slow===1)return true;
+    console.log(slow,fast)
+    return (slow!==fast);
+    
+    function run(num){
+        return num.toString().split("").map(x=>(+x)**2).reduce((ans,x)=>ans+x)
+    }
 };
