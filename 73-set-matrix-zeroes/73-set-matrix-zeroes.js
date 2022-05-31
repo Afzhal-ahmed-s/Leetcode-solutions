5e7cf107fb274f579460bcbd7aa9a0c2b3dcbdbd
Afzhal-ahmed-s <96117548+Afzhal-ahmed-s@users.noreply.github.com>
2022-05-31 09:40:54 +0530

    Time: 146 ms (16.55%), Space: 46.4 MB (9.93%) - LeetHub

---
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var setZeroes = function(matrix) {
+    let row=new Set(),col=new Set();
+    let m=matrix.length, n=matrix[0].length;
+    // console.log("length",l);
+    
+    for(let i=0;i<m;i++){
+        for(let j=0;j<n;j++){
+            // console.log("all",i,j);
+            if(matrix[i][j]==0){
+                row.add(i);
+                col.add(j);
+                // console.log(i,j);
+            }
+        }
+    }
+    // console.log(row,col);
+    
+    for(let i=0;i<m;i++){
+        if(row.has(i))matrix[i]=matrix[i].map(x=>x*0);
+    }
+    
+    for(let i=0;i<n;i++){
+        for(let j=0;j<m;j++){
+            if(col.has(i)){
+               matrix[j][i]=0;
+            }
+        }
+    }
+    
+    return matrix;
+};
