cc6929fc0d2e6d92edf571d5b5dc2e9eb5fe4fc6
Afzhal-ahmed-s <96117548+Afzhal-ahmed-s@users.noreply.github.com>
2022-06-02 12:08:20 +0530

    Time: 103 ms (57.43%), Space: 44.8 MB (53.54%) - LeetHub

--- 43-multiply-strings/43-multiply-strings.js
+++ 43-multiply-strings/43-multiply-strings.js
@@ -4,5 +4,17 @@
  * @return {string}
  */
 var multiply = function(num1, num2) {
-    return eval(BigInt(num1)*BigInt(num2)).toString();
+    // return eval(BigInt(num1)*BigInt(num2)).toString();
+    
+    let product = new Array(num1.length + num2.length).fill(0)
+    for (let i = num1.length; i--;) {
+        let carry = 0
+        for (let j = num2.length; j--;) {
+            product[1 + i + j] += carry + num1[i] * num2[j]
+            carry = Math.floor(product[1+i+j] / 10);
+            product[1 + i + j] = product[1 + i + j] % 10
+        }
+        product[i] += carry
+    }
+    return product.join("").replace(/^0*(\d)/, "$1");
 };
