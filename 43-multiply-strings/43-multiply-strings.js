91709a48961f613e69ffa4eee9f1d64bfe69ef85
Afzhal-ahmed-s <96117548+Afzhal-ahmed-s@users.noreply.github.com>
2022-06-02 12:30:06 +0530

    Time: 1090 ms (5.09%), Space: 51.1 MB (11.17%) - LeetHub

--- 43-multiply-strings/43-multiply-strings.js
+++ 43-multiply-strings/43-multiply-strings.js
@@ -7,10 +7,11 @@ var multiply = function(num1, num2) {
     // return eval(BigInt(num1)*BigInt(num2)).toString();
     
     let product = new Array(num1.length + num2.length).fill(0)
-    for (let i = num1.length; i--;) {
+    for (let i = num1.length-1;i>=0 ;i--) {
         let carry = 0
-        for (let j = num2.length; j--;) {
-            product[1 + i + j] += carry + num1[i] * num2[j]
+        for (let j = num2.length-1;j>=0; j--) {
+            product[1 + i + j] += carry + num1[i] * num2[j];
+            console.log(i+j+1);
             carry = Math.floor(product[1+i+j] / 10);
             product[1 + i + j] = product[1 + i + j] % 10
         }
