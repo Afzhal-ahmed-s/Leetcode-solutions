c9a5c0596b19e5829d6f816ededdf10df5d052e5
Afzhal-ahmed-s <96117548+Afzhal-ahmed-s@users.noreply.github.com>
2022-06-02 05:30:39 +0530

    Time: 67 ms (81.41%), Space: 42.4 MB (28.32%) - LeetHub

--- 50-powx-n/50-powx-n.js
+++ 50-powx-n/50-powx-n.js
@@ -3,6 +3,20 @@
  * @param {number} n
  * @return {number}
  */
-var myPow = function(x, n) {
-    return x**n;
+// var myPow = function(x, n) {
+//     return x**n;
+// };
+var myPow = function(a, b) {
+    
+  if(b==0)      return 1            // base case  
+  else if(b==1) return a           // base case 
+  else if(b==-1)  return 1/a
+  else if(b%2==0){
+  		let t=myPow(a, b/2);
+      return t*t
+  }
+  else if(b%2!=0){
+  		return a*myPow(a,b-1)
+  }
+
 };
