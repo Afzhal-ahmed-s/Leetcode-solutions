d0cc26ad4f3be5c7917d8e3009fd9f874d20ffb6
Afzhal Ahmed S <96117548+Afzhal-ahmed-s@users.noreply.github.com>
2022-10-07 17:30:53 +0530

    Time: 2 ms (87.50%), Space: 44.2 MB (49.15%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.java
+++ 90-subsets-ii/90-subsets-ii.java
@@ -1,6 +1,6 @@
-
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
         List<List<Integer>> answer = new ArrayList();
         List<Integer> temporary = new ArrayList<>();
         int depth = 0;
@@ -12,18 +12,16 @@ class Solution {
         int[] nums, List<Integer> temporary, int depth, List<List<Integer>> answer)
     {
         if(depth == nums.length){
-            List <Integer> temp = new ArrayList(temporary);
-            Collections.sort(temp);
-            if(!answer.contains(temp)){
-                answer.add(temp);  
-            }
-            
+            answer.add(new ArrayList(temporary)); 
             return;
         }
         else{
             temporary.add(nums[depth]);
             backTrack(nums, temporary, depth+1, answer);
             temporary.remove(temporary.size()-1);
+            while(depth < nums.length -1 && (nums[depth]==nums[depth+1])){
+                depth++;
+            }
             backTrack(nums, temporary, depth+1, answer);
         }
     }
