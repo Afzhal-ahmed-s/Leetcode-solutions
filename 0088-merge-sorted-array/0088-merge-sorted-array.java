39c928817bf5047034a9e40ca8a217b9c7a96303
Afzhal Ahmed S <96117548+Afzhal-ahmed-s@users.noreply.github.com>
2023-02-22 11:43:39 +0530

    Time: 0 ms (100.00%), Space: 42.9 MB (10.04%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -0,0 +1,37 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int[] dupNums1 = new int[m];
+        
+        for(int a=0; a<m; a++){
+            dupNums1[a] = nums1[a];
+        }
+        
+        int c1 = 0; int c2 = 0;
+        int run = 0;
+        
+        while(c1<m && c2<n){
+            if(dupNums1[c1] < nums2[c2]){
+                nums1[run] = dupNums1[c1];
+                c1++;
+            }
+            else {
+                nums1[run] = nums2[c2];
+                c2++;
+            }
+            run++;
+        }
+        //System.out.println(run+" "+c1+" "+m+" "+c2+" "+n);
+
+        while(c1 < m){
+            //System.out.println(run+" "+c1+" "+c2);
+            nums1[run] = dupNums1[c1];
+            run++; c1++;
+        }
+        
+        while(c2 < n){
+            nums1[run] = nums2[c2];
+            run++; c2++;
+        }
+        //System.out.println(run+" "+c1+" "+m+" "+c2+" "+n);
+}
+}
