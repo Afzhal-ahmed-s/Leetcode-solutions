9b2935693ac8032c894a657cf45657e945d432ac
Afzhal Ahmed S <96117548+Afzhal-ahmed-s@users.noreply.github.com>
2023-03-01 11:45:46 +0530

    Time: 1 ms (74.67%), Space: 40.4 MB (61.63%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -2,28 +2,15 @@ class Solution {
     public String longestCommonPrefix(String[] strs) {
         int len = strs.length;
         if(len==0)return "";
-        int lenStr1 = strs[0].length();
-        char[] firstString = strs[0].toCharArray();
-        int firstStringLen = firstString.length;
-        if(firstStringLen==0)return "";
-        int ans = Integer.MAX_VALUE;
-
-        if(len>1){
-        for(int a=1; a<len; a++){
-         int pointer = 0;
-            int currentStringLen= strs[a].length();
-            if(currentStringLen==0)return "";
-            int range = Math.min(firstStringLen, currentStringLen);
-            for(int b=0; b<range; b++){
-                if(strs[a].charAt(b)==firstString[b]){
-                    pointer++;
-                }
-                else break;
-            }
-            ans = Math.min(ans, pointer);
+        
+        Arrays.sort(strs);
+        int pointer = 0;
+        
+        while(pointer < strs[0].length()){
+            if(strs[0].charAt(pointer) == strs[len-1].charAt(pointer))pointer++;
+            else break;
         }
-        }
-        if(ans == Integer.MAX_VALUE)return strs[0];
-        return strs[0].substring(0,ans);
+        return strs[0].substring(0, pointer);
+        
     }
 }
