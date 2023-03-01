6d51c19212cac496391173a43ecdbbe66b87c50c
Afzhal Ahmed S <96117548+Afzhal-ahmed-s@users.noreply.github.com>
2023-03-01 11:32:00 +0530

    Time: 1 ms (74.67%), Space: 40.8 MB (38.51%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -0,0 +1,33 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        int len = strs.length;
+        if(len==0)return "";
+        int lenStr1 = strs[0].length();
+        char[] firstString = strs[0].toCharArray();
+        int firstStringLen = firstString.length;
+        if(firstStringLen==0)return "";
+        int ans = Integer.MAX_VALUE;
+        
+        //for(char c : firstString)System.out.print(c+" ");
+        //System.out.println();
+        if(len>1){
+        for(int a=1; a<len; a++){
+         int pointer = 0;
+            int currentStringLen= strs[a].length();
+            if(currentStringLen==0)return "";
+            int range = Math.min(firstStringLen, currentStringLen);
+            for(int b=0; b<range; b++){
+                if(strs[a].charAt(b)==firstString[b]){
+                    //System.out.println(strs[a].charAt(b)+" = "+firstString[b]);
+                    pointer++;
+                }
+                else break;
+            }
+            ans = Math.min(ans, pointer);
+            //System.out.println(a+" - "+pointer);
+        }
+        }
+        if(ans == Integer.MAX_VALUE)return strs[0];
+        return strs[0].substring(0,ans);
+    }
+}
