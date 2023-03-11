de3f03f33300950ed29db172961ceae8be890c5c
Afzhal Ahmed S <96117548+Afzhal-ahmed-s@users.noreply.github.com>
2023-03-11 12:48:48 +0530

    Time: 4 ms (45.46%), Space: 46.3 MB (43.02%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -4,25 +4,19 @@ class Solution {
         if(len==1 || len==2)return nums[0];
         
         Arrays.sort(nums);
-        int count=0; int bloodySweet = 0;
-        if(len%2==0)bloodySweet = len/2;
+        int count=1; int bloodySweet = 0;
+        if(len%2==0) bloodySweet = len/2;
         else bloodySweet = (len/2) + 1;
-        //System.out.println(bloodySweet);
-        //[2,3,3]
-        //[1,1,1,2,2,2,2]
         
         while(l<=r && r<len){
-            //System.out.println("check: "+l+" "+r);
           if(nums[l] == nums[r]){
-            //System.out.println("check2: "+l+" "+r);              
-              count++;
               while(r<len && nums[l]==nums[r]){
                   count++;
                   r++;
               }
            if(count >= bloodySweet)return nums[l]; 
         }
-            count=0;
+          count=1;
           l=r; r=l+1;  
     }
        return -1; 
