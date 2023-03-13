1f1e2cde68539b0be990a42438d18e8c5a0ce7fc
Afzhal Ahmed S <96117548+Afzhal-ahmed-s@users.noreply.github.com>
2023-03-13 14:30:33 +0530

    Time: 0 ms (100.00%), Space: 42 MB (35.51%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -4,9 +4,6 @@ class Solution {
         int len = nums.length;
         int s=0, e= len-1;
         
-        // 0,1,2,3   len=4, t=7
-        // 1,3,5,6
-        
         while(s <= e){
             int mid = e - (e-s)/2;
             if(nums[mid]==target)return mid;
