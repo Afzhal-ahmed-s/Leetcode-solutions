537bd26f32ca8e03c7e9f8a4dd3208c986cd05b9
Afzhal Ahmed S <96117548+Afzhal-ahmed-s@users.noreply.github.com>
2023-04-10 15:56:15 +0530

    Time: 1 ms (100.00%), Space: 40.1 MB (40.15%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -3,7 +3,6 @@ class Solution {
         
         int n = Math.abs(x);
         long ans = 0;
-        boolean endNumber = false;
         boolean negative = false;
         
         if(x < 0)negative = true;
@@ -11,11 +10,10 @@ class Solution {
         while(n > 0){
             int temp = n%10;
             n = n/10;
-            if(endNumber==false && temp != 0)endNumber = true;
-            if(endNumber)ans = (ans * 10) + temp;
+            ans = (ans * 10) + temp;
         }
         
         if(ans > Integer.MAX_VALUE || ans < Integer.MIN_VALUE)return 0;
-       return (negative == true) ? (int)-ans : (int)ans;
+        return (negative == true) ? (int)-ans : (int)ans;
     }
 }
