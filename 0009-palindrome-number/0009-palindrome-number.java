59cdec6c2f2daa98d0e75f2e80c0dd2d12f0c7de
Afzhal Ahmed S <96117548+Afzhal-ahmed-s@users.noreply.github.com>
2023-04-11 09:03:55 +0530

    Time: 9 ms (98.87%), Space: 42.1 MB (51.62%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -2,19 +2,19 @@ class Solution {
     public boolean isPalindrome(int x) {
         if(x < 0)return false;
         if(x!=0 && x/10 == 0)return true;
-
+        if(x!=0 && x%10 == 0)return false;
+        
         int revNum = 0;
         int n = x;
-        while(n > 0){
-                    //System.out.println(n+" - "+revNum);
+        
+        while(n > revNum){
             
             int lastDigit = n%10;
             revNum = revNum*10 + lastDigit;
             n= n/10;
         }
-        //System.out.println("Outside: "+n+" - "+revNum);
-
-        return (x == revNum) ? true : false;
+        //System.out.println("check: "+ n+", "+revNum);
+        return (n == revNum || n == revNum/10) ? true : false;
         
        
     }
