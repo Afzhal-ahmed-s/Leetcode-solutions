e3780ac3f9c92b5a6151e861828a7fafb73026fa
Afzhal Ahmed S <96117548+Afzhal-ahmed-s@users.noreply.github.com>
2023-04-14 15:06:19 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Frequencies of Limited Range Array Elements/frequencies-of-limited-range-array-elements.cpp
@@ -0,0 +1,96 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std; 
+
+// } Driver Code Ends
+class Solution{
+    public:
+    //Function to count the frequency of all elements from 1 to N in the array.
+    void frequencyCount(vector<int>& arr,int N, int P)
+    { 
+        // code here
+        int i = 0;
+        while (i<N)
+        {
+            // If this element is already processed,
+            // then nothing to do
+            if (arr[i] <= 0 || arr[i]>N || arr[i]>P)
+            {
+                i++;
+                continue;
+            }
+     
+            // Find index corresponding to this element
+            // For example, index for 5 is 4
+            int elementIndex = arr[i]-1;
+     
+            // If the elementIndex has an element that is not
+            // processed yet, then first store that element
+            // to arr[i] so that we don't lose anything.
+            if (arr[elementIndex] > 0)
+            {
+                arr[i] = arr[elementIndex];
+     
+                // After storing arr[elementIndex], change it
+                // to store initial count of 'arr[i]'
+                arr[elementIndex] = -1;
+            }
+            else
+            {
+                // If this is NOT first occurrence of arr[i],
+                // then decrement its count.
+                arr[elementIndex]--;
+     
+                // And initialize arr[i] as 0 means the element
+                // 'i+1' is not seen so far
+                arr[i] = 0;
+                i++;
+            }
+        }
+        for(int i=0;i<N;i++) {
+            if(arr[i] < 0) arr[i] = -arr[i];
+            else arr[i]=0;
+        }
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() 
+{ 
+	long long t;
+	
+	//testcases
+	cin >> t;
+	
+	while(t--){
+	    
+	    int N, P;
+	    //size of array
+	    cin >> N; 
+	    
+	    vector<int> arr(N);
+	    
+	    //adding elements to the vector
+	    for(int i = 0; i < N ; i++){
+	        cin >> arr[i]; 
+	    }
+        cin >> P;
+        Solution ob;
+        //calling frequncycount() function
+		ob.frequencyCount(arr, N, P); 
+		
+		//printing array elements
+	    for (int i = 0; i < N ; i++) 
+			cout << arr[i] << " ";
+	    cout << endl;
+	}	
+	return 0; 
+}
+
+
+
+
+
+// } Driver Code Ends
