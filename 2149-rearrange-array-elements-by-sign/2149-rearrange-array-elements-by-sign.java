70ac26baa3cb113751c2832280528fb73b167d97
Afzhal Ahmed S <96117548+Afzhal-ahmed-s@users.noreply.github.com>
2023-05-01 13:25:40 +0530

    Time: 9 ms (25.43%), Space: 73.8 MB (84.13%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
@@ -6,25 +6,19 @@ class Solution {
         int[] ans = new int[length];
         int positiveInsertionPointer = 0;
         int negativeInsertionPointer = 1;
-        boolean positiveMoved = false; boolean negativeMoved = false;
         
         while(positivePointer < length && negativePointer < length){
             while(positivePointer < length && nums[positivePointer] < 0 ){
                 positivePointer++;
-                positiveMoved = true;
             }
             while(negativePointer < length && nums[negativePointer] > 0 ){
                 negativePointer++;
-                negativeMoved = true;
             }
-                        System.out.println(positivePointer+", "+negativePointer);
 
             insert(ans, nums, positivePointer, negativePointer, positiveInsertionPointer, negativeInsertionPointer);
              positiveInsertionPointer += 2;
-        negativeInsertionPointer += 2;
-            //if(!positiveMoved)
+             negativeInsertionPointer += 2;
                 positivePointer++;
-            //if(!negativeMoved)
                 negativePointer++;
         }
         return ans;
